53d0aae4b044fca62b1b13b0a22091342c9eee52
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-17 20:31:59 +0530

    rectified variable type

--- staging/variables.tf
+++ staging/variables.tf
@@ -1,13 +1,16 @@
 variable "bucket_name" {
-    
+    type = list
     default = ["demobacket-tw-1","demobucket-tw-2"]
    
 }
 
 variable "aws_users" {
+  type = list
   default = ["ankit.tripathi@scaleworx.io"]
 }
 
-variable "devops_group_users" {
+variable "devops_group_users" 
+{
+  type = list
   default = ["ankit.tripathi@scaleworx.io"]
 }
