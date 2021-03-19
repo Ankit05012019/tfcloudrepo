2edf36a313520c586c5a39d928209a028e5c3835
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-19 10:03:47 +0530

    password in output
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-user-management/users.tf
+++ modules/aws-user-management/users.tf
@@ -1,5 +1,5 @@
 # User Creation
-resource "aws_iam_user" "knotch-aws-user" {
+resource "aws_iam_user" "aws-user" {
   for_each      = toset(var.aws_users)
   name          = "${each.key}"
   force_destroy = "false"
