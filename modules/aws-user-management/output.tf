01a20f4f4f2b1c9e368d6a04fe6cf73640aa640c
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-19 12:34:21 +0530

    password in output
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-user-management/output.tf
+++ modules/aws-user-management/output.tf
@@ -1,11 +1,13 @@
-output "group_ids" {
+/*output "group_ids" {
 
     value = aws_iam_group.aws-group
   
-}
+}*/
 
 output "password" {
 
 
-     value = aws_iam_user.aws-user.encrypted_password
+     value = tomap({
+    for name , user in aws_iam_user.aws-user : name => user.encrypted_password
+  })
 }
