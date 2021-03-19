023130434ec81d0ffc2ed0dd384e5170c3027014
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-19 11:00:38 +0530

    password in output
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-user-management/output.tf
+++ modules/aws-user-management/output.tf
@@ -1,11 +1,11 @@
 output "group_ids" {
 
-    value = aws_iam_group.aws-group[*]
+    value = aws_iam_group.aws-group
   
 }
 
 output "password" {
 
 
-     value = aws_iam_user.aws-user[*]
+     value = aws_iam_user.aws-user
 }
