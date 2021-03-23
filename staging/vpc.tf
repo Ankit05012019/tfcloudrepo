6e48a67acf032fc16df06f9efc993bfcab06e2d8
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-23 14:42:43 +0530

    adding vpc module
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

---
+++ staging/vpc.tf
@@ -0,0 +1,9 @@
+module "staging-vpc" {
+
+    source               = "../modules/aws-vpc"
+    cidr_block           = var.cidr
+    Name                 = var.name
+    environment          = var.environment
+    public-subnet        = var.public_subnets
+
+}
