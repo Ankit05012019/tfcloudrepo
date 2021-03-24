45f83ff53f445f59358af064f60d77073f2a6700
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-24 15:57:18 +0530

    adding db public subnets
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- staging/vpc.tf
+++ staging/vpc.tf
@@ -6,7 +6,7 @@ module "staging-vpc" {
     name                 =  var.name
     environment          =  var.environment
     public_subnets       =  var.public_subnets
-    private_subnets_db   =  var.private_subnet_db
+    private_subnets_db   =  var.private_subnets_db
     private_subnets_app  =  var.private_subnets_app
     enable_dns_hostnames =  var.enable_dns_hostnames
     enable_dns_support   =  var.enable_dns_support
