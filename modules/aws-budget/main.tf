2f072d18aac7bfa8d746baeb81efb57730a6165a
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-25 14:14:43 +0530

    adding budget module
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-budget/main.tf
+++ modules/aws-budget/main.tf
@@ -9,7 +9,7 @@ resource "aws_budgets_budget" "my-budget" {
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  =  90
+    threshold                  =  10
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses =  var.email_addresses
