3a2c4036edb21359643f1ce470cb06998306b618
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-26 15:36:56 +0530

    adding global resources
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- staging/vpc.tf
+++ staging/vpc.tf
@@ -6,8 +6,8 @@ module "staging-vpc" {
     name                 =  var.name
     environment          =  var.environment
     public_subnets       =  var.public_subnets
-    private_subnets_db   =  var.private_subnets_db
-    private_subnets_app  =  var.private_subnets_app
+    /*private_subnets_db   =  var.private_subnets_db
+    private_subnets_app  =  var.private_subnets_app*/
     enable_dns_hostnames =  var.enable_dns_hostnames
     enable_dns_support   =  var.enable_dns_support
 
