bef3fe6b1e467288c5b070b1dbf936dc1ea59ca1
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-03-26 16:07:08 +0530

    variables for global resources
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- staging/variables.tf
+++ staging/variables.tf
@@ -1,21 +1,3 @@
-variable "bucket_name" {
-    type = list
-    default = ["demobacket-tw-1","demobucket-tw-2"]
-   
-}
-
-variable "aws_users" {
-
-  type = list
-  default = ["ankit.tripathi@scaleworx.io","tfcloud","random","rohan"]
-
-}
-
-variable "devops_group_users" {
-
-  type = list
-  default = ["ankit.tripathi@scaleworx.io"]
-}
 
 variable "name" {
 
@@ -90,39 +72,3 @@ variable "private_subnets_db" {
 
 
 
-
-/* Budget Variables */ 
-
-variable "budget_name" {
-
-    type = string 
-    default = "tf-test-budget"
-}
-
-
-variable "budget_type" {
-
-    type = string 
-    default = "COST"
-}
-
-variable "limit_amount" {
-
-    type = number
-    default = 10
-
-}
-
-variable "time_unit" {
-
-     type = string
-     default = "MONTHLY"
-
-}
-
-variable "email_addresses" {
-
-     type = list
-     default = ["tripathi.ankitjnj@gmail.com"]
-
-}
