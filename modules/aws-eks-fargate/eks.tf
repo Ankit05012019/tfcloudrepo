631cb4c9239cbbc56ee4f508a62c8084e22177d4
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-04-02 01:20:34 +0530

    adding eks module
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-eks-fargate/eks.tf
+++ modules/aws-eks-fargate/eks.tf
@@ -1,7 +1,18 @@
 
 module "aws-vpc" {
 
-    source = "../aws-vpc"
+
+    source               =  "../modules/aws-vpc"
+    cidr                 =  var.cidr
+    name                 =  var.name
+    environment          =  var.environment
+    public_subnets       =  var.public_subnets
+    /*private_subnets_db   =  var.private_subnets_db
+    private_subnets_app  =  var.private_subnets_app*/
+    enable_dns_hostnames =  var.enable_dns_hostnames
+    enable_dns_support   =  var.enable_dns_support
+
+
 }
 
 
