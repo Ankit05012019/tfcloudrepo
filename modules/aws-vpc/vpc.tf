03fe7b1486bf15ebe53975e79c2a350c94570555
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-05-28 20:42:06 +0530

    reduce the number of NAT gateways
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-vpc/vpc.tf
+++ modules/aws-vpc/vpc.tf
@@ -109,7 +109,7 @@ resource "aws_route_table" "private-route-table-app" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id =  aws_nat_gateway.nat[each.key].id
+    nat_gateway_id =  aws_nat_gateway.nat.id
   }
 
   tags = {
