733130d335b3ffdb222bb996152b0114bee296ea
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-05-28 21:00:48 +0530

    fix - json error in fargate.tf
    Signed-off-by: Ankit05012019 <tripathi.ankit17@gmail.com>

--- modules/aws-eks-fargate/fargate.tf
+++ modules/aws-eks-fargate/fargate.tf
@@ -15,14 +15,14 @@ resource "aws_iam_role" "pod-execustion-role" {
   name               = format("tw-%s-fargate-%s", var.cluster_name, each.value)
   #assume_role_policy = data.aws_iam_policy_document.assume-role.json
   assume_role_policy    = <<POLICY
-  {
+ {
     "Version": "2012-10-17",
     "Statement": [
       {
         "Effect": "Allow",
         "Principal": {
           "Service": [
-            "eks-fargate-pods.amazonaws.com"
+            "eks-fargate-pods.amazonaws.com",
             ]
         },
         "Action": "sts:AssumeRole"
