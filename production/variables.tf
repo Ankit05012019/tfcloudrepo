42e2839f63e00680fc82c0bd958f0815d2591477
Ankit05012019 <tripathi.ankit17@gmail.com>
2021-06-14 13:49:30 +0530

    testing vpc peering

---
+++ production/variables.tf
@@ -0,0 +1,187 @@
+
+variable "name" {
+
+    type = string 
+    default = "prod-vpc"
+}
+
+variable "environment" {
+
+    type = string 
+    default = "eks-test-prod"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  type = string
+  default = "10.16.0.0/16"
+}
+
+variable "public_subnets" {
+
+  description = "A map of availability zones to public cidrs"
+  type        = "map"
+  default     = {
+    
+    us-east-1a = "10.16.16.0/20",
+    us-east-1b = "10.16.32.0/20",
+    us-east-1c = "10.16.48.0/20"  
+
+  }
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
+}
+
+
+variable "enable_dns_support" {
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
+}
+
+variable "private_subnets_app" {
+  description = "A map of availability zones to private cidrs"
+  type        = "map"
+  default     = {
+
+    us-east-1a = "10.16.128.0/20",
+    us-east-1b = "10.16.64.0/20",
+    us-east-1c = "10.16.96.0/20"
+
+  }
+}
+
+
+variable "private_subnets_db" {
+  description = "A map of availability zones to private cidrs"
+  type        = "map"
+  default     = {
+
+    us-east-1a = "10.16.80.0/24",
+    us-east-1b = "10.16.112.0/24",
+    us-east-1c = "10.16.144.0/24"
+
+  }
+}
+/*variable "public_propagating_vgws" {
+  description = "A list of VGWs the public route table should propagate."
+  default     = []
+}*/
+
+
+/*EKS variables */
+
+/*
+variable "cluster_name" {
+  type    = string
+  default = "tf-test-cluster"
+}
+
+variable "cluster_version" {
+  type    = number
+  default = 1.18
+}
+
+variable "cluster_service_ipv4_cidr" {
+  type    = string
+  default = "172.20.0.0/16"
+}
+
+variable "cluster_endpoint_private_access_cidrs" {
+  type    = list(any)
+  default = ["0.0.0.0/0"]
+}
+
+variable "eks_node_group_instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "cluster_endpoint_private_access" {
+  type    = bool
+  default = true
+}
+
+variable "cluster_endpoint_public_access" {
+  type    = bool
+  default = false 
+}
+
+
+variable "namespaces" {
+  type    = set(string)
+  default = ["fargate"]
+}
+variable "private_subnet_ids" {
+  type    = set(string)
+  default = []
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    name = "tf-test-cluster"
+  }
+
+}
+
+variable "vpc_id" {
+  type    = string
+  default = ""
+} 
+
+variable "enable_spot_instances" {
+
+  type = bool
+  default = true
+
+}
+
+variable "eks_spot_node_group_instance_types" {
+  type    = list(string)
+  default = ["t3.medium","t2.medium"]
+}
+
+variable "spot_node_group_desired" {
+
+  type = number
+  default = 1
+
+}
+
+variable "spot_node_group_max_size" {
+
+  type = number
+  default = 2
+  
+}
+ variable "spot_node_group_min_size" {
+
+  type = number
+  default = 1
+  
+}
+
+
+variable "node_group_desired" {
+
+  type = number
+  default = 1
+
+}
+
+variable "node_group_max_size" {
+
+  type = number
+  default = 2
+  
+}
+ variable "node_group_min_size" {
+
+  type = number
+  default = 1
+  
+}
+*/
---
+++ production/vpc.tf
@@ -0,0 +1,24 @@
+module "prod-vpc" {
+
+
+  source               = "../modules/aws-vpc"
+  cidr                 = var.cidr
+  name                 = var.name
+  environment          = var.environment
+  public_subnets       = var.public_subnets
+  private_subnets_db   = var.private_subnets_db
+  private_subnets_app  = var.private_subnets_app
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  cluster_name         = "test_cluster_prod"
+
+}
+
+resource "aws_vpc_peering_connection" "test-peering" {
+  peer_owner_id = var.peer_owner_id
+  peer_vpc_id   = module.staging-vpc.vpc-id
+  vpc_id        = module.prod-vpc.vpc-id
+  peer_region   = "us-east-1"
+
+  depends_on  = []
+}
